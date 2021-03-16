300f18662a62b9c30ff4b2ca16342eac28baf0fc
Dominic <dominicbarajas548@gmail.com>
2021-03-16 15:08:40 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function saturdayFun(target = "roller-skate") {
+    console.log(`This Saturday, I want to ${target}`)
+}
+
+saturdayFun("eat pudding.")
+saturdayFun()
+
+let mondayWork = function(target = "go to the office") {
+    console.log(`This Monday, I will ${target}.`)
+}
+
+mondayWork("go to wrigly")
+mondayWork()
+
+let wrapAdjective = function(style = "*") {
+    return function(adjective = "special") {
+        `You are ${style}${adjective}${style}!`
+    }
+}
